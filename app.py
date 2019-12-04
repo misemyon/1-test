cd262643fbd94e60eeb36ab931f57ffc59d897d9
Semyon <misemyon88@gmail.com>
2019-12-04 21:00:02 +0500

    add sqlite db

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask
 
+from models import db, Men
+from routes import api
+
 app = Flask(__name__)
-
-
-@app.route('/')
-def hello():
-    return 'Hello word'
-
+app.register_blueprint(api)
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+    db.session.add(Men(name='Bob'))
+    db.session.add(Men(name='Rocky'))
+    db.session.commit()
 
 if __name__ == '__main__':
     app.run()
---
+++ routes.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, jsonify
+
+from models import Men
+
+api = Blueprint('api', __name__)
+
+
+@api.route('/')
+def hello():
+    return jsonify([(lambda men: men.json())(men) for men in Men.query.all()])
