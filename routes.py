05c56a37727340e5b39c119053f68a64fdc6062f
Semyon <misemyon88@gmail.com>
2019-12-04 21:20:12 +0500

    add methods to api

--- routes.py
+++ routes.py
@@ -1,10 +1,23 @@
 from flask import Blueprint, jsonify
 
-from models import Men
+from models import Men, db
 
-api = Blueprint('api', __name__)
+api = Blueprint('api', __name__, url_prefix='/api')
 
 
-@api.route('/')
-def hello():
+@api.route('/mens')
+def get_mens():
     return jsonify([(lambda men: men.json())(men) for men in Men.query.all()])
+
+@api.route('/men/id/<int:men_id>')
+def get_men(men_id):
+    men = Men.query.get(men_id)
+    return jsonify(men.json()) if men else ''
+
+@api.route('/men/name/<string:men_name>')
+def put_men(men_name, pn):
+    men = Men(name=men_name)
+    db.session.add(men)
+    db.session.commit()
+    return jsonify(men.json())
+
