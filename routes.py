c6042c015b119f36ad9f23ade0c1b88d73348259
mischenko <mischenko@greendatasoft.ru>
2019-12-19 12:36:06 +0500

    add pos method to API

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
 
-from models import Employee, db
+from models import Employee, db, Position
 
 index = Blueprint('index', __name__, url_prefix='/')
 api = Blueprint('api', __name__, url_prefix='/api')
@@ -15,12 +15,19 @@ def get_employee(men_id):
     employee = Employee.query.get(men_id)
     return jsonify(employee.json()) if employee else ''
 
-@api.route('/employee/name/<string:employee_name>')
-def put_men(employee_name):
-    employee = Employee(name=employee_name)
+@api.route('/employee/name/<string:employee_name>/pos/<int:pos_id>')
+def put_men(employee_name, pos_id):
+    employee = Employee(name=employee_name, position_id=pos_id)
     db.session.add(employee)
     db.session.commit()
     return jsonify(employee.json())
+
+@api.route('/position/name/<string:pos_name>')
+def put_pos(pos_name):
+    position = Position(name=pos_name)
+    db.session.add(position)
+    db.session.commit()
+    return jsonify(position.json())
 
 @index.route('/')
 @index.route('/index')
